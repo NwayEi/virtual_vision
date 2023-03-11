838ef60c57052dc6eda94da4772bdb764de095af
Tan Hui Min <hmqui90@gmail.com>
2023-03-11 15:24:51 +0800

    my own branch

---
+++ yolov8 model to audio.py
@@ -0,0 +1,17 @@
+## pip install roboflow
+## pip install ultralytics
+
+# from ultralytics import YOLO
+
+# model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
+
+# # Use the model
+# # results = model('https://ultralytics.com/images/bus.jpg')  # predict on an image
+
+# print (f'this is result {model}')
+
+
+
+
+import ultralytics
+ultralytics.checks()
